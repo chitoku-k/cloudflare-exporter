7b3cc5b09e694db123199d10c0da3fe3dea6552c
Chitoku <odango@chitoku.jp>
2021-04-20 21:39:33 +0900

    Use signal.NotifyContext() for graceful shutdown

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"context"
-	"os"
 	"os/signal"
 	"syscall"
 
@@ -14,15 +13,8 @@ import (
 )
 
 func main() {
-	ctx, cancel := context.WithCancel(context.Background())
-
-	sig := make(chan os.Signal)
-	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
-	go func() {
-		defer close(sig)
-		<-sig
-		cancel()
-	}()
+	ctx, stop := signal.NotifyContext(context.TODO(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
 
 	env, err := config.Get()
 	if err != nil {
